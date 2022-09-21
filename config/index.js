8667a1fa82a5402ab21c69b5a433ac22445e6871
ygznb1 <108078494+ygznb1@users.noreply.github.com>
2022-09-21 09:47:44 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oRhjX6AKME_hP7SojZjmm2Uj8Ibk",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "TAwZ7Ce9AyVkHZsQ7EOF7_nm3wyNMrlkCKXBsceRImM	",
       // 所在省份
       province: "黑龙江省",
       // 所在城市
